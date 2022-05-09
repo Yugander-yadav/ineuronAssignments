5c336b838f88f8c0fd8dd61babf44b98476fc225
Yugander-yadav <yuganderpilli@outlook.com>
2022-05-10 03:16:25 +0530

    first assignment commit

---
+++ assignment1.py
@@ -0,0 +1,79 @@
+# 1. In the below elements which of them are values or an expression? eg:- values can be
+# integer or string and expressions will be mathematical operators.
+
+
+# *         -> Multiplication Operator
+# 'hello'   -> String
+# -87.8     -> Floating point number
+# -         -> Subraction Operator
+# /         -> Division Operator
+# +         -> Addition Operator 
+# 6         -> Integer Number 
+
+
+# 2. What is the difference between string and variable?
+
+# String : it is a collection of characters i.e alphabets, numbers, special characters etc 
+# Variable : it is a container in the memory  to which we can assign a value and perform action 
+
+
+
+# 3. Describe three different data types.
+
+# INT Data Type  : interger number ( with out decimal points )
+# Floating Point number : number with decimal points 
+# String : Collection of characters 
+# Bool :  Boolean Data type, Possible values : True / False 
+
+
+# 4. What is an expression made up of? What do all expressions do? 
+
+# Expressions are made up of both operators and operands used to perform an action / calculation from which we can obtain a value 
+
+
+# 5. This assignment statements, like spam = 10. What is the difference between an
+
+# # expression and a statement?
+
+# Expression contains only opeartors , Variables ( Operands) and an expression results a value 
+# example : 2+3
+# Statement is everthing that makes up  the code in the python code with; expression is also the part of Statement
+#  example : print(2+3)
+
+
+# 6. After running the following code, what does the variable bacon contain?
+
+
+# bacon = 22
+# bacon + 1
+
+# The bacon value will be 22 
+
+# 7. What should the values of the following two terms be?
+
+
+# 'spam'+'spamspam' =>  'spamspamspam'
+#  'spam'*3 => 'spamspamspam'
+
+
+
+# 8. Why is eggs a valid variable name while 100 is invalid?
+
+# Because the variable name cannot start with a number 
+
+
+
+# 9. What three functions can be used to get the integer, floating-point number, or string
+# version of a value?
+
+#  To get int version of a value we use  =====>  int(VALUE)
+#  To get float version of a value we use  =====>  float(VALUE)
+#  To get string version of a value we use =======> str(VALUE)
+
+
+# 10. Why does this expression cause an error? How can you fix it?
+
+#  'I have eaten' + 99 + ' burritos'
+
+# => because we cannot append integer with a string value 
+#  in the above example 'I have eaten ' and 'burritos ' is a string and 99 number hence we got the error 
